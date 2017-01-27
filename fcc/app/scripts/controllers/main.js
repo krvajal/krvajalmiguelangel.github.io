af8f4054b40d14eea41dec876046916eb2c1ee94
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2017-01-27 00:57:46 -0300

    Add about

---
+++ fcc/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pomodorTimerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pomodorTimerApp
+ */
+angular.module('pomodorTimerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
